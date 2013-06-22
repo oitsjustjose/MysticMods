b6de497e56ac5a8413390a731186cda06022b651
Roger Schoellgen <allstar602@gmai.com>
2013-06-21 19:24:54 -0700

    extends item now instead of the old ItemMysticWorld

--- common/MysticWorld/Items/ItemStaffAir.java
+++ common/MysticWorld/Items/ItemStaffAir.java
@@ -11,21 +11,22 @@ import net.minecraft.world.World;
 
 public class ItemStaffAir extends Item {
 
-	public ItemStaffAir(int id) {
+	public ItemStaffAir(int id) 
+	{
 		super(id);
         this.setMaxStackSize(1);
         this.setMaxDamage(64);
 		this.setCreativeTab(MysticWorld.MysticWorldTab);
 	}
 	
-	  @Override
-	    public void registerIcons(IconRegister iconRegister)
-	    {
-	             itemIcon = iconRegister.registerIcon("MysticTextures" + ":" + "item.AirStaff");
-	    }
-		
+	@Override
+	public void registerIcons(IconRegister iconRegister)
+	{
+	    itemIcon = iconRegister.registerIcon("MysticTextures" + ":" + "item.AirStaff");
+	}
 	
-	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer){
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer)
+	{
 		world.playAuxSFXAtEntity((EntityPlayer)null, 1009, (int)entityPlayer.posX, (int)entityPlayer.posY, (int)entityPlayer.posZ, 0);
 	
         if (!world.isRemote)
--- common/MysticWorld/Items/ItemStaffParts.java
+++ common/MysticWorld/Items/ItemStaffParts.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
@@ -16,7 +17,7 @@ import MysticWorld.Lib.Strings;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemStaffParts extends ItemMysticWorld {
+public class ItemStaffParts extends Item {
 
 	private Icon[] icons;
 	
