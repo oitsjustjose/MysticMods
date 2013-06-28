93ad480126aa9b5ea305b4415e5a40a3faa3ff06
allstar69 <allstar602@gmail.com>
2013-06-27 19:44:10 -0700

    Cleaned up energy orb names

--- common/MysticWorld/Items/ItemOrbEnergy.java
+++ common/MysticWorld/Items/ItemOrbEnergy.java
@@ -20,7 +20,7 @@ public class ItemOrbEnergy extends Item {
 		this.setCreativeTab(MysticWorld.MysticWorldTab);
 	}
 	
-    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    public boolean onItemUse(ItemStack itemStack, EntityPlayer entityPlayer, World world, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
     {
         if (par7 == 0)
         {
@@ -52,21 +52,21 @@ public class ItemOrbEnergy extends Item {
             ++par4;
         }
 
-        if (!par2EntityPlayer.canPlayerEdit(par4, par5, par6, par7, par1ItemStack))
+        if (!entityPlayer.canPlayerEdit(par4, par5, par6, par7, itemStack))
         {
             return false;
         }
         else
         {
-            int i1 = par3World.getBlockId(par4, par5, par6);
+            int i1 = world.getBlockId(par4, par5, par6);
 
             if (i1 == 0)
             {
-                par3World.playSoundEffect((double)par4 + 0.5D, (double)par5 + 0.5D, (double)par6 + 0.5D, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
-                par3World.setBlock(par4, par5, par6, BlockHandler.lightCube.blockID);
+                world.playSoundEffect((double)par4 + 0.5D, (double)par5 + 0.5D, (double)par6 + 0.5D, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+                world.setBlock(par4, par5, par6, BlockHandler.lightCube.blockID);
             }
 
-            par1ItemStack.damageItem(1, par2EntityPlayer);
+            itemStack.damageItem(1, entityPlayer);
             return true;
         }
     }
