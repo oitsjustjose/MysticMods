bae5961a48bddae064bdc8ee5184f1b9c3593f99
allstar69 <allstar602@gmail.com>
2013-07-23 08:47:37 -0700

    Air jump distance is now based off of how much the staff is charged up.

--- common/MysticWorld/Items/ItemStaffAir.java
+++ common/MysticWorld/Items/ItemStaffAir.java
@@ -30,47 +30,51 @@ public class ItemStaffAir extends ItemStaff {
 		
 		itemStack.stackTagCompound.setInteger("maxChargeTime", 3 * 20);
 						
-		if (!world.isRemote)
+		if (currentItem != null)
 		{
-			if (currentItem != null)
+			if (currentItem.itemID == itemStack.itemID)
 			{
-				if (currentItem.itemID == itemStack.itemID)
-				{
-					player.fallDistance = 0.0F;
+				player.fallDistance = 0.0F;
+				
+				if (!world.isRemote)
 					MysticWorld.proxy.airFeetFX(world, (player.posX - 0.5D) + rand.nextDouble(), player.posY + (player.height/itemStack.stackTagCompound.getInteger("maxChargeTime") * itemStack.stackTagCompound.getInteger("chargeTime")), (player.posZ - 0.5D) + rand.nextDouble(), 1.0f + ((float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (5.0f * (1 / (float)itemStack.stackTagCompound.getInteger("maxChargeTime")))));
 				
-					if (!Mouse.isButtonDown(1))
+				if (!Mouse.isButtonDown(1))
+				{
+					if (itemStack.stackTagCompound.getInteger("chargeTime") > 0)
 					{
-						if (itemStack.stackTagCompound.getInteger("chargeTime") > 0)
-						{
-							if (!(Keyboard.isKeyDown(Keyboard.KEY_RSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)))
+						if (!(Keyboard.isKeyDown(Keyboard.KEY_RSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)))
+						{							
+							if (!world.isRemote)
 							{
 								world.playAuxSFXAtEntity((EntityPlayer)null, 1009, (int)player.posX, (int)player.posY, (int)player.posZ, 0);
-						
-								if (!world.isRemote)
-								{
-									world.spawnEntityInWorld(new EntityChargeAir(world, player, 1.0f + ((float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (5.0f * (1 / (float)itemStack.stackTagCompound.getInteger("maxChargeTime")))), (float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (2.5D * (1.0D / (double)itemStack.stackTagCompound.getInteger("maxChargeTime")))));
-					        
-									itemStack.damageItem(1, player);
-								}	
+								world.spawnEntityInWorld(new EntityChargeAir(world, player, 1.0f + ((float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (5.0f * (1 / (float)itemStack.stackTagCompound.getInteger("maxChargeTime")))), (float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (2.5D * (1.0D / (double)itemStack.stackTagCompound.getInteger("maxChargeTime")))));
+								
+								itemStack.damageItem(1, player);	
+							}
+						}
+						else
+						{
+							if (player.onGround)
+							{
+								world.playSoundEffect(player.posX, player.posY, player.posZ, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+								itemStack.damageItem(1, player);
+								player.setVelocity(0.0D, 1.0D + (float)(itemStack.stackTagCompound.getInteger("chargeTime")) * (2.5D * (1.0D / (double)itemStack.stackTagCompound.getInteger("maxChargeTime"))), 0.0D);
 							}
 						}
 					}
-					
-					if (Mouse.isButtonDown(1))
+						
+					itemStack.stackTagCompound.setInteger("chargeTime", 0);
+					itemStack.stackTagCompound.setBoolean("charging", false);
+				}
+				else
+				{
+					if (itemStack.stackTagCompound.getBoolean("charging") == true)
 					{
-						if (itemStack.stackTagCompound.getBoolean("charging") == true)
-						{
-							itemStack.stackTagCompound.setInteger("chargeTime", itemStack.stackTagCompound.getInteger("chargeTime") + 1);
-						}
+						itemStack.stackTagCompound.setInteger("chargeTime", itemStack.stackTagCompound.getInteger("chargeTime") + 1);
 					}
-					else
-					{
-						itemStack.stackTagCompound.setInteger("chargeTime", 0);
-						itemStack.stackTagCompound.setBoolean("charging", false);
-					}	
-				}	
-			}
+				}
+			}	
 		}
 		
 		if (itemStack.stackTagCompound.getInteger("chargeTime") > itemStack.stackTagCompound.getInteger("maxChargeTime"))
@@ -88,16 +92,6 @@ public class ItemStaffAir extends ItemStaff {
 		if (itemStack.stackTagCompound.getBoolean("charging") == false)
 		{
 			itemStack.stackTagCompound.setBoolean("charging", true);
-		}
-		
-		if ((Keyboard.isKeyDown(Keyboard.KEY_RSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)))
-		{
-			if (player.onGround)
-			{
-				player.worldObj.playSoundEffect(player.posX, player.posY, player.posZ, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
-				itemStack.damageItem(1, player);
-				player.addVelocity(0, 2.5, 0);
-			}
 		}
 		
 		return itemStack;
